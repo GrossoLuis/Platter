db61a717baf848b8d4964a98fb66bb75a344abfa
GrossoLuis <luisgrossol@hotmail.com>
2025-01-28 12:09:18 -0300

    push

---
+++ platter.js
@@ -0,0 +1,18 @@
+Read the below instructions carefully:
+
+Create the design above with at least 10 Product Cards.On mobile only the first 4 should show, and the “Show More” Button should trigger a smooth dropdown revealing the other 6 products.
+
+Hover over the Image should swap the Image to a secondary Image.Please use Free Stock images as placeholders: https://unsplash.com/
+Using the following tech stack:
+
+- `HTML`
+- `JavaScript`
+- `TailwindCSS`
+
+The main objective of this tech challenge is to see your ability to create designs using `TailwindCSS` and well structured semantic `HTML`.
+
+The design needs to be fully responsive (desktop/mobile). Please use plain JS (VanillaJS) for any functionality. The scrollbar thumb should increase to 6px height when hovering / touching it, this will require hiding the default browser scrollbar. 
+
+The Show More button (mobile only) should trigger a dropdown revealing the other product cards.
+
+
